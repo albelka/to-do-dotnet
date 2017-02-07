6cd2e410fc2acd8fa904637af0610ac5873a148e
Anne Belka and Brad Copenhaver <anne+brad@>
2017-02-07 09:53:02 -0800

    add categories with one to many relationship and backup of database

--- src/todo/Models/ToDoListContext.cs
+++ src/todo/Models/ToDoListContext.cs
@@ -7,7 +7,8 @@ using Microsoft.EntityFrameworkCore;
 namespace ToDoList.Models
 {
     public class ToDoListContext : DbContext
-    {
+    {      
+        public virtual DbSet<Category> Categories { get; set; }
         public virtual DbSet<Item> Items { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
